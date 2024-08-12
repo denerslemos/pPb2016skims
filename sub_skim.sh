7426012940c2560a6d05fb01f37e066519a4345c
denerslemos <denerslemos@gmail.com>
2024-08-12 22:12:32 +0200

    bugfix

--- sub_skim.sh
+++ sub_skim.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 echo "Setup CMSSW (ROOT version)"
-cd /afs/cern.ch/user/d/ddesouza/CMSSW_12_5_0/src
+cd /afs/cern.ch/work/d/ddesouza/UIC/pPbMultAna/CMSSW_13_0_5/src/
 eval `scramv1 runtime -sh`
-cd /afs/cern.ch/work/d/ddesouza/news/pPbskims
-mkdir -p cond
+cd /afs/cern.ch/work/d/ddesouza/UIC/pPbMultAna/CMSSW_13_0_5/src/JetSkims/pPb2016skims
 echo "Submit skim jobs at "
 echo PWD: $PWD
 
-./pPbSkim $1 $2 $3 $4
+./pPbSkim $1 $2 $3 $4 $5
